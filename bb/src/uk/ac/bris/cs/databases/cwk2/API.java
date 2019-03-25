be74c8074ef40ac055488014cac16790a8485442
Tim van Ellemeet <timvanellemeet@gmail.com>
2019-03-25 11:49:48 +0000

    init commit

---
+++ bb/src/uk/ac/bris/cs/databases/cwk2/API.java
@@ -0,0 +1,135 @@
+package uk.ac.bris.cs.databases.cwk2;
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.Map;
+import uk.ac.bris.cs.databases.api.APIProvider;
+import uk.ac.bris.cs.databases.api.AdvancedForumSummaryView;
+import uk.ac.bris.cs.databases.api.AdvancedForumView;
+import uk.ac.bris.cs.databases.api.ForumSummaryView;
+import uk.ac.bris.cs.databases.api.ForumView;
+import uk.ac.bris.cs.databases.api.AdvancedPersonView;
+import uk.ac.bris.cs.databases.api.PostView;
+import uk.ac.bris.cs.databases.api.Result;
+import uk.ac.bris.cs.databases.api.PersonView;
+import uk.ac.bris.cs.databases.api.SimpleForumSummaryView;
+import uk.ac.bris.cs.databases.api.SimpleTopicView;
+import uk.ac.bris.cs.databases.api.TopicView;
+
+/**
+ *
+ * @author csxdb
+ */
+public class API implements APIProvider {
+
+    private final Connection c;
+    
+    public API(Connection c) {
+        this.c = c;
+    }
+
+    /* A.1 */
+    
+    @Override
+    public Result<Map<String, String>> getUsers() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Result<PersonView> getPersonView(String username) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    @Override
+    public Result addNewPerson(String name, String username, String studentId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    /* A.2 */
+
+    @Override
+    public Result<List<SimpleForumSummaryView>> getSimpleForums() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Result createForum(String title) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+ 
+    /* A.3 */
+ 
+    @Override
+    public Result<List<ForumSummaryView>> getForums() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }   
+    
+    @Override
+    public Result<ForumView> getForum(int id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }   
+
+    @Override
+    public Result<SimpleTopicView> getSimpleTopic(int topicId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }    
+    
+    @Override
+    public Result<PostView> getLatestPost(int topicId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Result createPost(int topicId, String username, String text) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+     
+    @Override
+    public Result createTopic(int forumId, String username, String title, String text) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    @Override
+    public Result<Integer> countPostsInTopic(int topicId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    /* B.1 */
+       
+    @Override
+    public Result likeTopic(String username, int topicId, boolean like) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    @Override
+    public Result likePost(String username, int topicId, int post, boolean like) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Result<List<PersonView>> getLikers(int topicId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Result<TopicView> getTopic(int topicId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    /* B.2 */
+
+    @Override
+    public Result<List<AdvancedForumSummaryView>> getAdvancedForums() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Result<AdvancedPersonView> getAdvancedPersonView(String username) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Result<AdvancedForumView> getAdvancedForum(int id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
