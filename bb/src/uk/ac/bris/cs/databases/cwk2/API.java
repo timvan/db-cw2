d2a381cd6e6ab50fb622760a97f5e228017d17eb
timvan <timvanellemeet@gmail.com>
2019-04-15 10:31:12 +0100

    changes

--- bb/src/uk/ac/bris/cs/databases/cwk2/API.java
+++ bb/src/uk/ac/bris/cs/databases/cwk2/API.java
@@ -1,6 +1,8 @@
 package uk.ac.bris.cs.databases.cwk2;
 
-import java.sql.Connection;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import uk.ac.bris.cs.databases.api.APIProvider;
@@ -31,18 +33,74 @@ public class API implements APIProvider {
     /* A.1 */
     
     @Override
-    public Result<Map<String, String>> getUsers() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public Result<Map<String, String>> getUsers(){
+
+        String query = "SELECT username, name FROM Person";
+
+        try (PreparedStatement s = c.prepareStatement(query)) {
+            Map<String, String> users = new HashMap<>();
+            ResultSet r = s.executeQuery();
+            while(r.next()){
+                String name = r.getString("name");
+                String username = r.getString("username");
+                users.put(username, name);
+            }
+            s.close();
+
+            return Result.success(users);
+
+        } catch (SQLException e) {
+            return Result.fatal("SQL error...");
+        } catch (Exception e) {
+            return Result.fatal("Could not get users");
+        }
     }
 
     @Override
     public Result<PersonView> getPersonView(String username) {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        String query = ("SELECT * FROM Person WHERE username = ?");
+
+        try (PreparedStatement s = c.prepareStatement(query)) {
+            s.setString(1, username);
+            ResultSet r = s.executeQuery();
+            r.next(); // SHOULD WE CHECK IF HAS NEXT BEFORE?
+            PersonView person = new PersonView(
+                    r.getString("name"), r.getString("username"), r.getString("stuId")
+            );
+            s.close();
+            return Result.success(person);
+        } catch (SQLException e){
+
+            return Result.fatal("Could not get user");
+        }
+
+        // TODO should we handle a fatal error for other exceptions?
     }
     
     @Override
     public Result addNewPerson(String name, String username, String studentId) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        String query = ("INSERT INTO Person (name, username, stuId) VALUES (?,?,?)");
+
+        if(name == null || name.isEmpty()) return Result.failure("Name cannot be null or empty");
+        if(username == null || username.isEmpty()) return Result.failure("username cannot be null or empty");
+        if(studentId == null || studentId.isEmpty()) return Result.failure("StudentId cannot be null or empty");
+
+        // TODO HOW IS UNIQUENESS HANDLED???
+
+        try (PreparedStatement s = c.prepareStatement(query)) {
+            s.setString(1, name);
+            s.setString(2, username);
+            s.setString(3, studentId);
+
+            ResultSet r = s.executeQuery();
+            s.close();
+            return Result.success();
+
+        } catch (SQLException e) {
+            return Result.fatal("Could not create user");
+        }
+
     }
     
     /* A.2 */
